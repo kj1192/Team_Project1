eafe31c7422736c86ceba3e519c48693c9f5ff1a
yusw10 <39454792+yusw10@users.noreply.github.com>
2018-09-22 10:40:59 +0000

    Ras-Arduino v1.2

--- t1.py
+++ t1.py
@@ -1,15 +1,16 @@
 import serial
 import requests
-
+import urllib.request
 port = "/dev/ttyACM1"
 serialFromArduino = serial.Serial(port,9600)
 
 serialFromArduino.flushInput()
 while True:
-	input_s = serialFromArduino.readline()
-	durl = 'http://10.0.2.15'
-	payload = {'tmp':input_s.decode()[:-1]}
+	input_s = serialFromArduino.readline().decode()[:-1]
+	#durl = 'http://10.0.2.15?tmp='+input_s.decode()[:-1]
+	#payload = {'tmp':input_s.decode()[:-1]}
+	#r = requests.get(durl)
 
-	r = requests.get(durl,params = payload)
+	send = uirllib.request.Request(url='http://10.0.2.15',data = inpu_s,method = 'PUT', headers = 'tmp')
 	print("end r");
 	print(r.text)
