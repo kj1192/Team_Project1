687c485361ba87c3bae42dd536e7ec4eb66332bc
jungmineeee <39642305+jungmineeee@users.noreply.github.com>
2018-09-22 17:34:18 +0900

    Add files via upload
    When the http request is GET
    then save the data and httpResponse

--- project/views.py
+++ project/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Mytable
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 import json
 
 
 def index(request):
-    
-
+    if request.method == "GET"
     try :
-        context = {'time' : time, 'temperature' : temp, 'status' : stat}
-        context.save()
+        tmp = str(request.read())
+        time = datetime.now()
+        stat = 0
+        if int(tmp) > 25:
+            stat = 1
+        else:
+            stat = 0
+        in_data = Mytable(time = time, tmp = tmp, stat = stat)
+        in_data.save()
     except :
-        context = None
-    return render(request, "project/", context)
+        in_data = None
+    return HttpResponse(in_data)
