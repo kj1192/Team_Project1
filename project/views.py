b51eabd66d58586b4750994846c7d8790bb652e4
jungmineeee <39642305+jungmineeee@users.noreply.github.com>
2018-09-22 23:15:05 +0900

    Add files via upload

--- project/views.py
+++ project/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Mytable
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 import json
 
 def index(request):
     if request.method == "GET":
-        try:
-            tmp = str(request.read())
-            stat = 0
+        try :
+            tmp = str(request.get_full_path())
+            tmp = tmp[1:-1]
+            stat = "OFF"
+            time = str(datetime.today().strftime("%Y/%m/%d %H:%M:%S  "))
+
             if int(tmp) > 25:
-                stat = 1
+                stat = "ON"
             else:
-                stat = 0
+                stat = "OFF"
             in_data = Mytable(tmp = tmp, stat = stat)
             in_data.save()
-        except:
+        except :
             in_data = None
+    
     return HttpResponse(in_data)
-def test(request):
-    return HttpResponse("This is that")
+
+def present(request):
+    rows = Mytable.objects.order_by('id').last()
+    time = str(rows.time.strftime("%Y/%m/%d %H:%M:%S "))
+    return HttpResponse(time + str(rows.tmp) +" " + str(rows.stat))
+
